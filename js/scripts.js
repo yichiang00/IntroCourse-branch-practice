78e05b721f37a40cd4129ecb334e45a1dc23e66f
Andrew Niekamp and Yi Chiang <andrew+yi@>
2016-06-16 10:08:29 -0700

    Add functions for form behavior and added images

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,31 @@
+var choice;
+var heightInput;
 $(function() {
-  $("#animalForm").submit(function() {
-    var choice = parseInt($("select").val());
-    if () {
-
+  $("#animalForm").submit(function(event) {
+    event.preventDefault();
+    choice = parseInt($("select").val());
+    if (choice === 1) {
+      window.open("https://simple.wikipedia.org/wiki/Snake", "_blank");
+    } else if (choice === 2) {
+      window.open("https://simple.wikipedia.org/wiki/Insect", "_blank");
+    } else {
+      window.open("https://simple.wikipedia.org/wiki/Turtle", "_blank");
     }
   });
+  $("#heightForm").submit(function(event) {
+    event.preventDefault();
+    $(".children").hide();
+    $(".teen").hide();
+    $(".adult").hide();
+    heightInput=$("input:radio[name=height]:checked").val();
+    if (heightInput === "short") {
+      $(".children").show();
+    } else if (heightInput === "medium") {
+      $(".teen").show();
+    } else {
+      $(".teen").show();
+      $(".adult").show();
+    }
+  });
+
 });
